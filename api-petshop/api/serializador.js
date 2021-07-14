283338715b6be01f171e17baa063ba7a8340fab2
Giovanni <giovanni.marcos.leite@gmail.com>
2021-07-14 18:39:11 -0300

    trabalhando com xml alem de json

--- api-petshop/api/serializador.js
+++ api-petshop/api/serializador.js
@@ -1,17 +1,37 @@
 const ValorNaoSuportado = require("./erros/valornaosuportado")
+const jsontoxml = require('jsontoxml')
 
 class Serializador{
     json (dados){
         return JSON.stringify(dados)
     }
+    xml(dados){
+        let tag = this.tagSingular
+
+        if(Array.isArray(dados)){
+            tag = this.tagPlural
+            dados = dados.map((item)=>{
+                return {[this.tagSingular]: item}
+            })
+        }
+
+        return jsontoxml({[tag]:dados})
+    }
+
+
     serializar (dados){
+        dados = this.filtrar(dados)
+
         if(this.contentType === 'application/json'){
-            return this.json(
-                this.filtrar(dados)
+            return this.json(dados
             )
-        }else{
+        }
+        if(this.contentType === 'application/xml'){
+            return this.xml(dados)
+            
+        }
             throw new ValorNaoSuportado(this.contentType)
-        }}
+    }
         
         
         filtrarObjeto(dados){
@@ -41,9 +61,11 @@ class Serializador{
 class SerializadorFornecedor extends Serializador{
     constructor(contentType, camposextras){
         super()
+        this.contentType = contentType
 
         this.campospublicos =['id', 'empresa', 'categoria'].concat(camposextras||[])
-        this.contentType = contentType
+        this.tagSingular = 'fornecedor'
+        this.tagPlural = 'fornecedores'
 
     }
 }
@@ -56,6 +78,8 @@ class SerializadorErro extends Serializador{
             'mensagem',
             'id'
         ].concat(camposextras || [])
+        this.tagSingular = 'erro'
+        this.tagPlural = 'erros'
     }
 }
 
@@ -63,5 +87,5 @@ module.exports = {
     SerializadorFornecedor: SerializadorFornecedor,
     Serializador: Serializador,
     SerializadorErro: SerializadorErro,
-    formatosaceitos: ['application/json']
+    formatosaceitos: ['application/json', 'application/xml']
 }
